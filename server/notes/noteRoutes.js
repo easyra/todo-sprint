3d4aeeb8922f2d1f46120b1b8a54eeb0acb1d9d2
ezra davis davis <ezra.rr.davis@gmail.com>
2018-12-16 07:54:43 -0800

    GETs finished on backend

---
+++ server/notes/noteRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const notesDb = require('./noteDbModel.js');
+
+router.get('/all', (req, res) => {
+  notesDb
+    .get()
+    .then(notes => {
+      res.status(200).json(notes);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  notesDb
+    .getById(id)
+    .then(note => {
+      res.status(200).json(note);
+    })
+    .catch(err => {
+      releaseEvents.json(err);
+    });
+});
