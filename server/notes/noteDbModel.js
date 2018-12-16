355de8ef78168250fdffcae9cb914256164625e0
ezra davis davis <ezra.rr.davis@gmail.com>
2018-12-16 07:43:55 -0800

    added CRUDS to db model

--- server/notes/noteDbModel.js
+++ server/notes/noteDbModel.js
@@ -1,1 +1,38 @@
 const knex = require('knex');
+const knexConfig = require('../knexfile');
+const db = knex(knexConfig['development']);
+const table = 'notes';
+
+module.exports = {
+  get,
+  getById,
+  add,
+  update,
+  remove,
+};
+
+function get() {
+  return db(table);
+}
+
+function getById(id) {
+  return db(table)
+    .where({ id })
+    .first();
+}
+
+function add(newData) {
+  return db(table).insert(newData);
+}
+
+function update(newData, id) {
+  return db(table)
+    .where({ id })
+    .update(newData);
+}
+
+function remove(id) {
+  return db(table)
+    .where({ id })
+    .del();
+}
