ade3b8bbfe7bdd328b0a2c2c2b7aa1b790f25a9f
ezra davis davis <ezra.rr.davis@gmail.com>
2018-12-16 09:03:46 -0800

    HomeCoatainer now gets notes from axios call

--- client/src/components/HomeContainer.js
+++ client/src/components/HomeContainer.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import NotesForm from './NotesForm';
+import NotesList from './NotesList';
+import Navigator from './Navigator';
 
 class HomeContainer extends Component {
+  state = {
+    notes: [],
+  };
   render() {
     return (
       <>
@@ -11,6 +17,12 @@ class HomeContainer extends Component {
       </>
     );
   }
+  componentDidMount = () => {
+    axios.get('http://localhost:6666/notes/all').then(res => {
+      const notes = res.data;
+      this.setState({ notes });
+    });
+  };
 }
 
 export default HomeContainer;
