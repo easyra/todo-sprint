5573955bf1f7740e7082c40bfe46ca53862be2b0
ezra davis davis <ezra.rr.davis@gmail.com>
2018-12-17 15:15:18 -0800

    NoteCard style changed

--- client/src/components/NoteCard.js
+++ client/src/components/NoteCard.js
@@ -3,10 +3,12 @@ import React from 'react';
 const NoteCard = ({ note }) => {
   const { title, content } = note;
   return (
-    <div className="col s12 m4">
-      <div className="card-panel purple z-depth-2">
-        <h4>{title}</h4>
-        <p>{content}</p>
+    <div className="col l3 s6 m4">
+      <div className="card purple z-depth-2">
+        <div className="card-content white-text">
+          <span className="card-title">{title}</span>
+          <p className="white-text">{content}</p>
+        </div>
       </div>
     </div>
   );
