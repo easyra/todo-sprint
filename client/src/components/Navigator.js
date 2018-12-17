7f01b49c857dfbc1c8ab682f6c5c3ab350d123a2
ezra davis davis <ezra.rr.davis@gmail.com>
2018-12-17 08:47:53 -0800

    navigator Styled

--- client/src/components/Navigator.js
+++ client/src/components/Navigator.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Navigator = props => {
   return (
-    <nav>
-      <h1>Notes</h1>
+    <nav className="purple">
+      <div className="nav-wrapper">
+        <div className="container">
+          <a className="brand-logo">Notes</a>
+          <ul id="nav-mobile" className="right hide-on-small-and-down">
+            <li>
+              <a>Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
