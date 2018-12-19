64c93eade75bbe3d949c3a54cb6bc184e2abf9fb
ezra davis davis <ezra.rr.davis@gmail.com>
2018-12-18 18:00:52 -0800

    login /logout inside navigator component

--- client/src/components/Navigator.js
+++ client/src/components/Navigator.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import Auth from './Auth.js';
+
+const auth = new Auth();
 
 const Navigator = props => {
+  const login = () => {
+    auth.handleAuthentication(auth.login());
+  };
+
+  const logout = () => {
+    auth.logout();
+  };
+  console.log(auth.getIdToken());
+
   return (
     <nav className="purple">
       <div className="nav-wrapper">
@@ -8,7 +20,10 @@ const Navigator = props => {
           <a className="brand-logo">Notes</a>
           <ul id="nav-mobile" className="right hide-on-small-and-down">
             <li>
-              <a>Login</a>
+              <a onClick={login}>Login</a>
+            </li>
+            <li>
+              <a onClick={logout}>Log out</a>
             </li>
           </ul>
         </div>
